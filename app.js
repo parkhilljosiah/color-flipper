a852212e18327245ca32d24d765693a885500462
Josiah Parkhill <parkhilljosiah@gmail.com>
2023-12-20 14:36:32 -0500

    final project

---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  //get random number between 0-3
+  const randomColor = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomColor];
+  color.textContent = colors[randomColor];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
